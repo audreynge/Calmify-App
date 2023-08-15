e4c70de49ff5ab3ce20d6d9d975cfb96304b740f
Audrey Ng <audrey518@gmail.com>
2023-08-15 11:03:58 -0700

    views

---
+++ Calmify App/MentalHealthInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  MentalHealthInfoView.swift
+//  Calmify App
+//
+//  Created by Audrey Ng on 8/15/23.
+//
+
+import SwiftUI
+
+struct MentalHealthInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MentalHealthInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        MentalHealthInfoView()
+    }
+}
---
+++ Calmify App/StressTipsView.swift
@@ -0,0 +1,20 @@
+//
+//  StressTipsView.swift
+//  Calmify App
+//
+//  Created by Audrey Ng on 8/15/23.
+//
+
+import SwiftUI
+
+struct StressTipsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StressTipsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StressTipsView()
+    }
+}
