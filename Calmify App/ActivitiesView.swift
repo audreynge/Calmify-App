59f3377a997a424d703aaf4944f64c328da44190
Audrey Ng <audrey518@gmail.com>
2023-08-15 11:43:22 -0700

    Updated files

--- Calmify App/ActivitiesView.swift
+++ Calmify App/ActivitiesView.swift
@@ -12,7 +12,34 @@ struct ActivitiesView: View {
                     .edgesIgnoringSafeArea(.all)
                 
                 Image("Choose your activity")
-                    .position(x: UIScreen.main.bounds.width * 0.35, y: UIScreen.main.bounds.height * 0.21)
+                    .position(x: UIScreen.main.bounds.width * 0.35, y: UIScreen.main.bounds.height * 0.18)
+                
+                NavigationLink(destination:BreathingView()){
+                    Image("breathingexercisescloud")
+                        .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                        .position(x: UIScreen.main.bounds.width * 0.27, y: UIScreen.main.bounds.height * 0.38)
+                }
+                    
+                NavigationLink(destination:StressTipsView()){
+                    Image("stresstipscloud")
+                        .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                        .position(x: UIScreen.main.bounds.width * 0.65, y: UIScreen.main.bounds.height * 0.52)
+                }
+                
+                NavigationLink(destination:QuotesView()){
+                    Image("quotescloud")
+
+                        .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                        .position(x: UIScreen.main.bounds.width * 0.3, y: UIScreen.main.bounds.height * 0.69)
+  
+                }
+                
+                NavigationLink(destination:MentalHealthInfoView()){
+                    Image("mentalhealthinfocloud")
+                        .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                        .position(x: UIScreen.main.bounds.width * 0.7, y: UIScreen.main.bounds.height * 0.84)
+  
+                }
                 
             }
         }
--- Calmify App/StressTipsView.swift
+++ Calmify App/StressTipsView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct StressTipsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            Image("bluebg")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            .clipped()
+            .edgesIgnoringSafeArea(.all)
+            
+            Image("Stress Management Tips")
+                .position(x: UIScreen.main.bounds.width * 0.43, y: UIScreen.main.bounds.height * 0.19)
+            
+            
+            Image("1-5 Scale Question")
+                .position(x : 190, y : 350)
+        }
+        VStack {
+            Button("Tired") {
+                
+            }
+
+        }
     }
 }
 
