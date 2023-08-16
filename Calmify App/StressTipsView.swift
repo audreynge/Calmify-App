f6acfc99c1751f59eaec888a4c53e722e62ce41f
Audrey Ng <audrey518@gmail.com>
2023-08-16 13:06:40 -0700

    Fixed StressTipsView

--- Calmify App/StressTipsView.swift
+++ Calmify App/StressTipsView.swift
@@ -10,52 +10,59 @@ struct StressTipsView: View {
       .clipped()
       .edgesIgnoringSafeArea(.all)
       Image("Stress Management Tips")
-        .position(x: UIScreen.main.bounds.width * 0.43, y: UIScreen.main.bounds.height * 0.19)
+        .position(x: UIScreen.main.bounds.width * 0.43, y: UIScreen.main.bounds.height * 0.16)
       Image("1-5 Scale Question")
-        .position(x : 190, y : 300)
+        .position(x : 190, y : 290)
       
         //Tired
         Button() {
-          let tiredTipsArray = ["Eat often to have more energy", "Even if you feel to tired, get moving, regular exercise will make you feel less tired", "Sleep well, avoid taking naps in the day", "Drink more water,Sometimes you feel tired simply because you're mildly dehydrated"]
+          let tiredTipsArray = ["Eat a snack!", "Get moving! Regular exercise will make you feel less tired.", "Sleep well; avoid taking naps in the day", "Drink more water. Sometimes you feel tired simply because you're mildly dehydrated."]
           let randomtiredTips = Int.random(in: 0..<tiredTipsArray.count)
           tips = tiredTipsArray[randomtiredTips]
           print(tiredTipsArray[randomtiredTips])
         }
           label: {
               Image("N1")
+                  .position(x : 39, y :43)
+              Text("Tired")
+                  .font(.custom("Heiti TC", size: 25))
+                  .foregroundColor(Color("lightpurple"))
+                  .position(x : -33, y :34)
           }
-          .position(x: UIScreen.main.bounds.width * 0.18, y: UIScreen.main.bounds.height * 0.5)
-        
-          Text("Tired")
-              .font(.title)
-              .fontWeight(.medium)
-              .foregroundColor(Color("lightpurple"))
+          .frame(width: 330,height:70)
+          .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.475)
           
-          
+
           //Tense
           Button() {
               _ = ["Watch something funny", "Drink green tea, it can make you relax","Organise your space, mess can really start to clutter up your mind","Light some candles, candlelight is known for its calming effects "]
           }
           label: {
               Image("N2")
+                  .position(x : 39, y :43)
               Text("Tense")
-                .font(.title)
-                .fontWeight(.medium)
+                .font(.custom("Heiti TC", size: 25))
                 .foregroundColor(Color("lightpurple"))
+                .position(x : -28, y :34)
            }
-          .position(x: UIScreen.main.bounds.width * 0.28, y: UIScreen.main.bounds.height * 0.58)
+          .frame(width: 330,height:70)
+          .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.56)
+          
         
         //anxious
-            Button() {
-            }
+          Button() {
+          }
           label: {
               Image("N3")
+                  .position(x : 39, y :43)
               Text("Anxious")
-                .font(.title)
-                .fontWeight(.medium)
+                .font(.custom("Heiti TC", size: 25))
                 .foregroundColor(Color("lightpurple"))
+                .position(x : -16, y : 35)
            }
-          .position(x: UIScreen.main.bounds.width * 0.31, y: UIScreen.main.bounds.height * 0.665)
+          .frame(width: 330,height:70)
+          .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.645)
+          
         
         //nervous
             Button() {
@@ -63,27 +70,37 @@ struct StressTipsView: View {
             }
           label: {
               Image("N4")
+                  .position(x : 39, y :40)
               Text("Nervous")
-                .font(.title)
-                .fontWeight(.medium)
+                .font(.custom("Heiti TC", size: 25))
                 .foregroundColor(Color("lightpurple"))
+                .position(x : -16, y :33)
            }
-          .position(x: UIScreen.main.bounds.width * 0.32, y: UIScreen.main.bounds.height * 0.75)
+          .frame(width: 330,height:70)
+          .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.73)
+          
         
         
             Button() {
             }
-          label: {
-              Image("N5")
-              Text("Overwhelmed")
-                .font(.title)
-                .fontWeight(.medium)
-                .foregroundColor(Color("lightpurple"))
-           }
-          .position(x: UIScreen.main.bounds.width * 0.41, y: UIScreen.main.bounds.height * 0.835)
-          
+            label: {
+                Image("N5")
+                    .position(x : 39, y : 45)
+                Text("Overwhelmed")
+                    .font(.custom("Heiti TC", size: 25))
+                    .foregroundColor(Color("lightpurple"))
+                    .position(x : 13, y :38)
+               }
+            .frame(width: 330,height:70)
+            .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.809)
+        
+                
         Text("\(tips)")
-            .position(x :200, y : 770)
+            .multilineTextAlignment(.center)
+            .font(.custom("Heiti TC", size: 20))
+            .foregroundColor(Color("lightblue"))
+            .frame(width: 330,height:100)
+            .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.9)
     }
   }
 }
@@ -92,12 +109,6 @@ struct StressTipsView_Previews: PreviewProvider {
     StressTipsView()
   }
 }
-
-
-
-
-
-
 
 
 
