3af61293faa82dc1562589ebef48a53a54a42c4a
Audrey Ng <audrey518@gmail.com>
2023-08-16 08:52:44 -0700

    Positioned clouds

--- Calmify App/QuotesView.swift
+++ Calmify App/QuotesView.swift
@@ -16,7 +16,7 @@ struct QuotesView: View {
             let quoteArray = ["Two roads diverged in a wood, and I, I took the road less traveled by - “The Road Not Taken” Robert Fros ", "The secret of getting ahead is getting started – Mark Twain", "The best way to get started is to quit talking and begin doing – Walt Disney", "The only impossible journey is the one you never begin – Tony Robbins", "When you know what you want, and want it bad enough, you’ll find a way to get it – Jim Rohn", "Never let the fear of striking out keep you from playing the game – Babe Ruth", "If you genuinely want something, don’t wait for it – teach yourself to be impatient – Gurbaksh Chahal", "The most difficult thing is the decision to act; the rest is merely tenacity – Amelia Earhart", "You don’t have to be great to start, but you have to start to be great – Zig Ziglar", "I believe that the only courage anybody ever needs is the courage to follow your dreams – Oprah Winfrey", "We generate fears while we sit. We overcome them by action – Dr. Henry Link", "All our dreams will come true, if we have the courage to pursue them – Walt Disney", "Whatever the mind can conceive and believe, it can achieve – Napoleon Hill"]
             let randomQuote = Int.random(in:0..<quoteArray.count)
             print(quoteArray[randomQuote])
-          quote = quoteArray[randomQuote] 
+            quote = quoteArray[randomQuote] 
         }
       label: {
         Image("QuoteGenerator")
--- Calmify App/StressTipsView.swift
+++ Calmify App/StressTipsView.swift
@@ -13,61 +13,77 @@ struct StressTipsView: View {
         .position(x: UIScreen.main.bounds.width * 0.43, y: UIScreen.main.bounds.height * 0.19)
       Image("1-5 Scale Question")
         .position(x : 190, y : 300)
-      VStack(alignment: .leading, spacing:1) {
+      
+        //Tired
         Button() {
           let tiredTipsArray = ["Eat often to have more energy", "Even if you feel to tired, get moving, regular exercise will make you feel less tired", "Sleep well, avoid taking naps in the day", "Drink more water,Sometimes you feel tired simply because you're mildly dehydrated"]
           let randomtiredTips = Int.random(in: 0..<tiredTipsArray.count)
           tips = tiredTipsArray[randomtiredTips]
           print(tiredTipsArray[randomtiredTips])
         }
-        label: {
-          Image("N1")
+          label: {
+              Image("N1")
+          }
+          .position(x: UIScreen.main.bounds.width * 0.18, y: UIScreen.main.bounds.height * 0.5)
+        
           Text("Tired")
-           .font(.title)
-           .fontWeight(.medium)
-          .foregroundColor(Color("lightpurple"))
-       }
-         Button() {
-         _ = ["Watch something funny", "Drink green tea, it can make you relax","Organise your space, mess can really start to clutter up your mind","Light some candles, candlelight is known for its calming effects "]
-        }
-      label: {
-          Image("N2")
-          Text("Tense")
-            .font(.title)
-            .fontWeight(.medium)
-            .foregroundColor(Color("lightpurple"))
-       }
-        Button() {
-        }
-      label: {
-          Image("N3")
-          Text("Anxious")
-            .font(.title)
-            .fontWeight(.medium)
-            .foregroundColor(Color("lightpurple"))
-       }
-        Button() {
-          _ = ["Close your eyes and count to 10 slowly", "Pop on your headphones and listen to some calming music","If you have time, do some meditation", "If you are nervous about future events, write down your thoughts and how can you prepare yourself for it"]
-        }
-      label: {
-          Image("N4")
-          Text("Nervous")
-            .font(.title)
-            .fontWeight(.medium)
-            .foregroundColor(Color("lightpurple"))
-       }
-        Button() {
-        }
-      label: {
-          Image("N5")
-          Text("Overwhelmed")
-            .font(.title)
-            .fontWeight(.medium)
-            .foregroundColor(Color("lightpurple"))
-       }
+              .font(.title)
+              .fontWeight(.medium)
+              .foregroundColor(Color("lightpurple"))
+          
+          
+          //Tense
+          Button() {
+              _ = ["Watch something funny", "Drink green tea, it can make you relax","Organise your space, mess can really start to clutter up your mind","Light some candles, candlelight is known for its calming effects "]
+          }
+          label: {
+              Image("N2")
+              Text("Tense")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundColor(Color("lightpurple"))
+           }
+          .position(x: UIScreen.main.bounds.width * 0.28, y: UIScreen.main.bounds.height * 0.58)
+        
+        //anxious
+            Button() {
+            }
+          label: {
+              Image("N3")
+              Text("Anxious")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundColor(Color("lightpurple"))
+           }
+          .position(x: UIScreen.main.bounds.width * 0.31, y: UIScreen.main.bounds.height * 0.665)
+        
+        //nervous
+            Button() {
+              _ = ["Close your eyes and count to 10 slowly", "Pop on your headphones and listen to some calming music","If you have time, do some meditation", "If you are nervous about future events, write down your thoughts and how can you prepare yourself for it"]
+            }
+          label: {
+              Image("N4")
+              Text("Nervous")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundColor(Color("lightpurple"))
+           }
+          .position(x: UIScreen.main.bounds.width * 0.32, y: UIScreen.main.bounds.height * 0.75)
+        
+        
+            Button() {
+            }
+          label: {
+              Image("N5")
+              Text("Overwhelmed")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundColor(Color("lightpurple"))
+           }
+          .position(x: UIScreen.main.bounds.width * 0.41, y: UIScreen.main.bounds.height * 0.835)
+          
         Text("\(tips)")
-      }
-      .position(x :160, y : 560)
+            .position(x :200, y : 770)
     }
   }
 }
