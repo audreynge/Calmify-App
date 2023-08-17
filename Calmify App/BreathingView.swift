e885d397b1de856ba4b1032a971a78e92f1abb53
Audrey Ng <audrey518@gmail.com>
2023-08-17 07:44:44 -0700

    Update

--- Calmify App/BreathingView.swift
+++ Calmify App/BreathingView.swift
@@ -1,8 +1,58 @@
 import SwiftUI
 
 struct BreathingView: View {
+    
+    @State var isCenter = true
+    @State var isCenter2 = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            
+            Image("bluebg")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .clipped()
+                .edgesIgnoringSafeArea(.all)
+            
+            Circle()
+            .frame(width:100,height:100)
+                .foregroundColor(Color("lightpurple").opacity(0.5))
+                .offset(x: isCenter ? 0 : 50)
+                .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+            
+            Circle()
+            .frame(width:100,height:100)
+                .foregroundColor(Color("lightpurple").opacity(0.5))
+                .offset(x: isCenter ? 0 : -50)
+                .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+            
+            Circle()
+            .frame(width:100,height:100)
+                .foregroundColor(Color("lightpurple").opacity(0.5))
+                .offset(y: isCenter ? 0 : 50)
+                .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+            
+            Circle()
+            .frame(width:100,height:100)
+                .foregroundColor(Color("lightpurple").opacity(0.5))
+                .offset(y: isCenter ? 0 : -50)
+                .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+   
+        }
+         
+        .onAppear(){
+            let animation = Animation.linear(duration: 5)
+            withAnimation(animation.repeatForever(autoreverses: true)){
+                self.isCenter = false
+                
+            }
+            
+            withAnimation(animation.repeatForever(autoreverses: true)){
+                self.isCenter2 = false
+            }
+            
+        }
     }
 }
 
--- Calmify App/ContentView.swift
+++ Calmify App/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
                 }
                 
             }
+            .navigationBarHidden(true)
         }
     }
 }
